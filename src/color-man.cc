9d6d042921bafb74679bde29104b58c636ca77d5
Alberto Griggio <griggio@fbk.eu>
2024-10-09 16:33:22 +0200

    color-man: enable black point compensation

--- src/color-man.cc
+++ src/color-man.cc
@@ -193,7 +193,7 @@ static ColorManCache *color_man_cache_new(ColorManProfileType in_type, const gch
 					   (has_alpha) ? TYPE_RGBA_8 : TYPE_RGB_8,
 					   cc->profile_out,
 					   (has_alpha) ? TYPE_RGBA_8 : TYPE_RGB_8,
-					   options->color_profile.render_intent, 0);
+					   options->color_profile.render_intent, cmsFLAGS_BLACKPOINTCOMPENSATION);
 
 	if (!cc->transform)
 		{
